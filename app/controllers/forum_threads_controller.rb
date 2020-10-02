52ecd26566f3d85e9b698cdd6f77259c7adf0c93
3lviend <henryss_10@yahoo.com>
2020-10-02 13:49:10 +0700

    action_create

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -1,9 +1,30 @@
 class ForumThreadsController < ApplicationController
 	def index
-		@threads = ForumThread.all
+		@threads = ForumThread.order(id: :desc)
 	end
 
 	def show
 		@thread = ForumThread.find(params[:id])
+	end
+
+	def new
+		@thread = ForumThread.new
+	end
+
+	def create
+		@thread = ForumThread.new(resource_params)
+		@thread.user = User.first
+		if @thread.save
+			puts "berhasil disimpan"
+		else
+			puts @thread.errors.full_messages
+		end
+		redirect_to root_path
+	end
+
+	private
+
+	def resource_params
+		params.require(:forum_thread).permit(:title, :content)
 	end
 end
